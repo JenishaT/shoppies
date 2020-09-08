9cd74317ce753f1aa6f61e1bf9781fb498cf4def
unknown <jenishathomas00@gmail.com>
2020-09-08 08:03:51 -0400

    Updated server.js

--- src/redux/movie/movie.actions.js
+++ src/redux/movie/movie.actions.js
@@ -2,13 +2,13 @@ import MOVIE_ACTION_TYPES from "./movie.action.types";
 import axios from "axios";
 
 export const searchMovies = (query) => async (dispatch) => {
-    const path = "http://www.omdbapi.com/?type=movie&s=" + query + "&apikey=2ae73c8e";
+    const path = "http://www.omdbapi.com/?apikey=2ae73c8e&type=movie&s=" + query;
     const { data } = await axios.get(path);
     dispatch({ type: MOVIE_ACTION_TYPES.MOVIE_SEARCH_SUCCESS, payload: data });
 }
 
 export const getMovieInfo = (id) => async (dispatch) => {
-    const path = "http://www.omdbapi.com/?plot=short&i=" + id + "&apikey=2ae73c8e";
+    const path = "http://www.omdbapi.com/?apikey=2ae73c8e&plot=short&i=" + id;
     const { data } = await axios.get(path);
     dispatch({ type: MOVIE_ACTION_TYPES.RETRIEVE_MOVIE_PLOT, payload: data });
 }
@@ -16,7 +16,7 @@ export const getMovieInfo = (id) => async (dispatch) => {
 export const addNomination = (id) => async (dispatch, getState) => {
     let nominations = getState().movie.nominations;
     if (!nominations.some(nomination => nomination.imdbID === id) || nominations.length === 0) {
-        const path = "http://www.omdbapi.com/?plot=short&i=" + id + "&apikey=2ae73c8e";
+        const path = "http://www.omdbapi.com/?apikey=2ae73c8e&plot=short&i=" + id;
         const { data } = await axios.get(path);
         nominations.push(data);
         dispatch({ type: MOVIE_ACTION_TYPES.ADD_NOMINATION_SUCCESS, payload: nominations });
