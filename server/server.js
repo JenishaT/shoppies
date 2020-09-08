0348bcd8a9f91505cb8d0c6ae0feafe5ce7a61f1
unknown <jenishathomas00@gmail.com>
2020-09-08 07:47:31 -0400

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+const app = require("./app");
+const express = require("express");
+const path = require("path");
+
+//Server serves react app in production build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server Online on Port ${process.env.PORT}`)
+);
